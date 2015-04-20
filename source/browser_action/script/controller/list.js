230d658b1de5676432f06357b75ba58ea2dcbf59
iamchristopher <chriswright@live.com>
2015-04-19 22:20:49 -0400

    Active tasks now use boolean flag for tracking

--- source/browser_action/script/controller/list.js
+++ source/browser_action/script/controller/list.js
@@ -1,7 +1,7 @@
 time_tracker.controller('list_controller', function($scope, task_service) {
 
     $scope.trackTask = function(id) {
-        task_service.startTracking(id);
+        task_service.setTaskAsActive(id);
     }
     
     $scope.init = function() {
--- source/browser_action/script/resource/task.js
+++ source/browser_action/script/resource/task.js
@@ -10,11 +10,41 @@ time_tracker.factory('task_service', function($resource, $interval, $filter) {
 
     service.addTask = function(data) {
         data.id = _tasks.length + 1;
+        data.active = true;
+
         _tasks.push(data);
-        this.startTracking(data.id);
+
+        this.setTaskAsActive(data.id);
     }
 
-    service.startTracking = function(id) {
+    service.setTaskAsActive = function(id) {
+        var task = getTaskByID(id),
+            active_task = getActiveTask();
+
+        active_task.active = false;
+
+        startTracking(id);
+        task.active = true;
+    }
+
+    function incrementTaskDuration(id) {
+        var task = getTaskByID(id);
+        task.duration++;
+    }
+
+    function getTaskByID(id) {
+        return $filter('filter')(_tasks, {
+            id: id
+        })[0];
+    }
+
+    function getActiveTask() {
+        return $filter('filter')(_tasks, {
+            active: true
+        })[0];
+    }
+
+    function startTracking(id) {
         if (_timer) {
             $interval.cancel(_timer);
             _timer = null;
@@ -25,12 +55,7 @@ time_tracker.factory('task_service', function($resource, $interval, $filter) {
         }, 1000)
     }
 
-    service.stopTracking = function() {
-    }
-
-    function incrementTaskDuration(id) {
-        var task = $filter('filter')(_tasks, {id: id})[0];
-        task.duration++;
+    function stopTracking(id) {
     }
 
     return service;

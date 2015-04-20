08446be70b3063dc64902d8706d2269baea97306
iamchristopher <chriswright@live.com>
2015-04-19 21:29:08 -0400

    Added method to add tasks to task resource

--- source/browser_action/script/resource/task.js
+++ source/browser_action/script/resource/task.js
@@ -4,17 +4,13 @@ time_tracker.factory('task_service', function($resource, $q) {
         _tasks = [];
 
     service.getTasks = function() {
-        // for (var i = 0; i < 5; i++) {
-        //     _tasks.push({
-        //         id: i,
-        //         name: 'Making time tracking extension',
-        //         elapsed_time: '--:--:--'
-        //     });
-        // }
+        return _tasks.reverse();
+    }
 
-        return _tasks;
+    service.addTask = function(data) {
+        _tasks.push(data);
     }
 
     return service;
 
-})
+});

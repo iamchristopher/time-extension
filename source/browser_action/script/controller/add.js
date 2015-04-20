070bcd1f0b4535f7f75430613ea2824fd7a795d9
iamchristopher <chriswright@live.com>
2015-04-19 21:29:50 -0400

    Changed add task form can now actually add tasks

--- source/browser_action/script/controller/add.js
+++ source/browser_action/script/controller/add.js
@@ -1,6 +1,24 @@
-time_tracker.controller('add_controller', function($scope) {
+time_tracker.controller('add_controller', function($scope, task_service) {
+
+    $scope.processTask = function() {
+        if ($scope.new_task.$invalid) {
+            return false;
+        }
+
+        var task_data = {
+            name: $scope.new_task.name,
+            notes: $scope.new_task.notes,
+            duration: 1200
+        }
+
+        addTask(task_data);
+    }
     
     $scope.init = function() {
     }();
+
+    function addTask(data) {
+        task_service.addTask(data);
+    }
 
 });

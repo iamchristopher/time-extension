eab65edbac98c6de133a23401c483314e71f2e55
iamchristopher <chriswright@live.com>
2015-04-19 23:22:14 -0400

    Fixed duration filter showing NaN

--- source/browser_action/script/filter/duration.js
+++ source/browser_action/script/filter/duration.js
@@ -1,6 +1,10 @@
 time_tracker.filter('duration', function() {
 
     return function(input) {
+        if (isNaN(input)) {
+            input = 0;
+        }
+
         var s = input % 60,
             m = Math.floor(input / 60) % 60,
             h = Math.floor(input / (60 * 60));

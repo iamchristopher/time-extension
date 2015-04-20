0215113fcf9409a5763993c3803db151a3fa729f
iamchristopher <chriswright@live.com>
2015-04-19 22:02:39 -0400

    Active tasks can now be toggled from list view

--- source/browser_action/script/controller/list.js
+++ source/browser_action/script/controller/list.js
@@ -1,4 +1,8 @@
-time_tracker.controller('list_controller', function($scope) {
+time_tracker.controller('list_controller', function($scope, task_service) {
+
+    $scope.trackTask = function(id) {
+        task_service.startTracking(id);
+    }
     
     $scope.init = function() {
     }();
